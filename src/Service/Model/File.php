68d0083d9b892870a5e9eb1eef74309c1318bebd
Ritesh Agarwal <ritesh@appycodes.com>
2025-11-13 04:17:26 +0000

    Updates to file handler

--- src/Service/Model/File.php
+++ src/Service/Model/File.php
@@ -26,27 +26,20 @@ final class File
     private $absolutePath;
 
     /**
-     * @var string
-     */
-    private $fileName;
-
-    /**
      * File constructor.
      *
      * @param string $name         Name of the file to be used as the payload key.
      * @param string $absolutePath Absolute file path. Ex: /tmp/test-image.jpg
-     * @param string $fileName     [optional] Name of the file to be used instead the actual file name
      *
      * @throws InvalidArgumentException when file is not found or readable.
      */
-    public function __construct($name, $absolutePath, $fileName = null)
+    public function __construct($name, $absolutePath)
     {
         if (!is_readable($absolutePath)) {
             throw new InvalidArgumentException(sprintf('File is not readable at location \"%s\"', $absolutePath));
         }
         $this->name = $name;
         $this->absolutePath = $absolutePath;
-        $this->fileName = $fileName;
     }
 
     /**
@@ -70,16 +63,6 @@ final class File
     }
 
     /**
-     * Returns the name of the file to be used instead the actual file name.
-     *
-     * @return string
-     */
-    public function getFileName()
-    {
-        return $this->fileName;
-    }
-
-    /**
      * Returns a array representation of the file with file contents as a resource.
      *
      * @return array
@@ -89,7 +72,6 @@ final class File
         return array(
             'name' => $this->getName(),
             'contents' => fopen($this->getAbsolutePath(), 'r'),
-            'filename' => $this->getFileName(),
         );
     }
 }
