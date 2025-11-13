d47850afe5a36fb66c729b432c451e94f41cd03f
Ritesh Agarwal <ritesh@appycodes.com>
2025-11-13 04:15:38 +0000

    Additions to event functions like guest, decline, approve, etc

--- src/Service/EventRequestService.php
+++ src/Service/EventRequestService.php
@@ -27,16 +27,54 @@ class EventRequestService extends TokenRefreshingService
         return $this->createResponse($this->put(self::BASE . "/{$event_id}/invite"));
     }
 
+    public function guestEventRegistration($payload)
+    {
+        return $this->createResponse($this->postFormData(self::BASE . "/guest-event-registration", $payload));
+    }
+    
     public function requestEventParticipation($event_id)
     {
         $event_id = intval($event_id);
         return $this->createResponse($this->postFormData(self::BASE . "/{$event_id}/request"));
     }
     
-    public function getEventRequestInvitation($group_id)
+    public function getEventRequestInvitation($group_id, $event_id = false)
     {
         $group_id = intval($group_id);
-        return $this->createResponse($this->get(self::BASE . "/request-and-invites/list/group/{$group_id}"));
+        if ($event_id){
+            return $this->createResponse($this->get(self::BASE . "/request-and-invites/list/group/{$group_id}/{$event_id}"));
+        } else {
+            return $this->createResponse($this->get(self::BASE . "/request-and-invites/list/group/{$group_id}"));
+        }
+    }
+
+    public function checkIn($event_id, $guest_id, $type)
+    {
+        $event_id = intval($event_id);
+        $guest_id = intval($guest_id);
+        return $this->createResponse($this->put(self::BASE . "/{$event_id}/checkin/$guest_id/type/$type"));
+    }
+
+    public function declineGuestEventInvitationRequest($event_id, $guest_id, $action)
+    {
+        $event_id = intval($event_id);
+        $guest_id = intval($guest_id);
+        return $this->createResponse($this->put(self::BASE . "/{$event_id}/guest-request/{$guest_id}/action/{$action}"));
+    }
+
+    public function declineEventInvitationRequestv2($event_id, $guest_id, $action)
+    {
+        $event_id = intval($event_id);
+        $guest_id = intval($guest_id);
+        return $this->createResponse($this->put(self::BASE . "/{$event_id}/request/{$guest_id}/action/{$action}"));
+    }
+    
+
+    public function approveGuestEventInvitationRequest($event_id, $guest_id)
+    {
+        $event_id = intval($event_id);
+        $guest_id = intval($guest_id);
+        return $this->createResponse($this->put(self::BASE . "/{$event_id}/guest-request/{$guest_id}"));
     }
 
     public function approveEventInvitationRequest($event_id, $recipient_id)
@@ -46,6 +84,13 @@ class EventRequestService extends TokenRefreshingService
         return $this->createResponse($this->put(self::BASE . "/{$event_id}/request/{$recipient_id}"));
     }
 
+    public function markGuestRequestSpam($event_id)
+    {
+        $event_id = intval($event_id);
+        return $this->createResponse($this->delete(self::BASE . "/mark-spam-request/{$event_id}"));
+    }
+
+    /* OLD */
     public function declineEventInvitationRequest($event_id, $recipient_id)
     {
         $event_id = intval($event_id);
@@ -53,3 +98,4 @@ class EventRequestService extends TokenRefreshingService
         return $this->createResponse($this->delete(self::BASE . "/{$event_id}/request/{$recipient_id}"));
     }
 }
+
